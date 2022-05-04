0cb23df430c52a11f170330c4ea3567871d6e218
Rob Relyea <rob@relyeas.net>
2022-05-04 13:09:16 -0700

    sort testToTreat.csv

--- src/transform-mAbs-data.py
+++ src/transform-mAbs-data.py
@@ -16,8 +16,25 @@ def sortDataFile(fileName):
 
   dataFile.to_csv(fileName, index=False)
 
+def sortTestToTreatFile(fileName):
+  dataFile = pd.read_csv(fileName)
+  print ("sorting: " + fileName)
+  
+  # ensure standard casing, so that sorting works better.
+  dataFile['city'] = dataFile['city'].str.title()
+
+  dataFile.sort_values(["state", "city", "zip", "provider_name"], 
+                      axis=0,
+                      ascending=[True, True, True, True], 
+                      inplace=True)
+
+  dataFile.to_csv(fileName, index=False)
+
 
 if len(sys.argv) > 1:
   dataFile = sys.argv[1]
 
-sortDataFile(dataFile)
+if "testToTreat" in dataFile:
+  sortTestToTreatFile(dataFile)
+else:
+  sortDataFile(dataFile)
